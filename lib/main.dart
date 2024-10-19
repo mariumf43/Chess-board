6d8427b036439a3866c024a6c4e5beb41cabe474
mariumf43 <mariumf43@gmail.com>
2024-10-19 13:58:01 +0500

    'commit'

---
+++ lib/main.dart
@@ -0,0 +1,428 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: const Text(
+            'Chess Board',
+            style: TextStyle(
+              color: Colors.white,
+              fontStyle: FontStyle.italic, // Change text color to white
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
+        body: Row(
+          children: [
+            // column 1
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            // column 2
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+               
+              ],
+            ),
+            // column 3
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            // column 4
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            // column 5
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            // column 6
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+               
+              ],
+            ),
+            // column 7
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                
+              ],
+            ),
+            // column 8
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            // column 5
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            // column 6
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+               
+              ],
+            ),
+            // column 5
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            // column 6
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+               
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
